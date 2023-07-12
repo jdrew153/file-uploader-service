4f946b3f2f47cbf8ca3b4fabfc52d08fa531cb28
jdrew153 <57649297+jdrew153@users.noreply.github.com>
2023-07-12 13:41:47 -0400

    Update main.go

--- main.go
+++ main.go
@@ -130,6 +130,17 @@ func main() {
 	http.HandleFunc("/", HandleGetFile)
 
 	http.HandleFunc("/download", func(w http.ResponseWriter, r *http.Request) {
+
+			// Enable CORS headers
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+		// Handle preflight OPTIONS request
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
 		
 		var body DownloadRequest
 
