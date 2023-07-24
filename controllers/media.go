90d1ac9baebf744dab14e93ba1971441ddb212f8
jdrew153 <57649297+jdrew153@users.noreply.github.com>
2023-07-23 22:59:35 -0400

    Update media.go

--- controllers/media.go
+++ controllers/media.go
@@ -94,7 +94,7 @@ err := r.ParseMultipartForm(2 << 30) // 32 MB max memory limit for parsing the f
 		return
 	}
 
-	log.Printf("File uploaded successfully! Size: %.2f MB", float64(written)/(1<<20))
+	log.Printf("File uploaded successfully!)
 
 	w.WriteHeader(http.StatusCreated)
 }
